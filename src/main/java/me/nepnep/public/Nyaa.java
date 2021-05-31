89b5d6d22bdd0712506dd8aa0aeebb78dd3c41a8
BitcoinJake09 <54378383+BitcoinJake09@users.noreply.github.com>
2021-05-31 14:07:35 -0400

    new functions sysOut
    use sysOut(<String>) instead of System.out.println :D

--- src/main/java/me/nepnep/public/Nyaa.java
+++ src/main/java/me/nepnep/public/Nyaa.java
@@ -1,5 +1,9 @@
 public class Nyaa {
     public static void main(String[] args) {
         System.out.println("Nyaa~ :3");
+        sysOut("BitcoinJake09 was here :p");
+    }
+    public void sysOut(String textOut) {
+        System.out.println(textOut);
     }
 }
