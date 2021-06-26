7698fbc776d5b33b714c7a9154f2132bf600ce94
mocehm <83355743+mocehm@users.noreply.github.com>
2021-06-26 22:20:58 +0200

    Create my discord bot token.txt.py

---
+++ my discord bot token.txt.py
@@ -0,0 +1,3 @@
+þis is my discord bot token. I want to know if discord invalidates it
+NzgxMDg0Mzc1NDg4MDA0MTA5.X74fgg.lZ6oUNg7dvpEYrXttAmj1C80sPo
+client.login("NzgxMDg0Mzc1NDg4MDA0MTA5.X74fgg.lZ6oUNg7dvpEYrXttAmj1C80sPo")
