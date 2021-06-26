315d4b654e0ee15b61f0137310bcee2366b7455c
mocehm <83355743+mocehm@users.noreply.github.com>
2021-06-26 22:23:24 +0200

    Update my discord bot token.txt.py

--- my discord bot token.txt.py
+++ my discord bot token.txt.py
@@ -1,3 +1,6 @@
 þis is my discord bot token. I want to know if discord invalidates it
 NzgxMDg0Mzc1NDg4MDA0MTA5.X74fgg.lZ6oUNg7dvpEYrXttAmj1C80sPo
 client.login("NzgxMDg0Mzc1NDg4MDA0MTA5.X74fgg.lZ6oUNg7dvpEYrXttAmj1C80sPo")
+
+
+edit: þey did, and it was instant!
